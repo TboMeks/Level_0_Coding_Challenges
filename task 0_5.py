2aecb51c98fae18c3a949edc907c607ca4da18e6
TboMeks <91221940+TboMeks@users.noreply.github.com>
2022-05-17 13:18:09 +0200

    Update task 0_5.py

--- task 0_5.py
+++ task 0_5.py
@@ -1,8 +1,8 @@
 def area_of_a_triangle(side1, side2, side3):
-    area_of_a_triangle = (side1 + side2 + side3) / 2
-    area = (s*(s-side1)*(s-side2)*(s-side3))**0.5
+    s = (side1 + side2 + side3) / 2
+    area = (s*(s-side1)*(s-side2)*(s-side3)) ** 0.5
 
     return area
 
 
-area_of_a_triangle(3, 4, 5)
+print(area_of_a_triangle(3, 4, 5))
