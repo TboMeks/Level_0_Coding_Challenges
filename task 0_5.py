d562317a99ab532070a447e830a1a8dd70a0c2fc
TboMeks <91221940+TboMeks@users.noreply.github.com>
2022-05-19 11:02:27 +0200

    Update task 0_5.py

--- task 0_5.py
+++ task 0_5.py
@@ -1,8 +1,9 @@
 def area_of_a_triangle(side1, side2, side3):
-    s = (side1 + side2 + side3) / 2
-    area = (s*(s-side1)*(s-side2)*(s-side3)) ** 0.5
+    semiperimeter = (side1 + side2 + side3) / 2
+    area_of_a_triangle = (semiperimeter*(semiperimeter-side1)
+                          * (semiperimeter-side2) * (semiperimeter-side3)) ** 0.5
 
-    return area
+    return area_of_a_triangle
 
 
 print(area_of_a_triangle(3, 4, 5))
